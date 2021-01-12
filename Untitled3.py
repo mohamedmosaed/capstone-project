68d90b9c94eb85211a17f040c45741cc9fe7a375
mohamedmosaed <59778377+mohamedmosaed@users.noreply.github.com>
2021-01-12 08:47:08 -0500

    final projecr.py

---
+++ Untitled3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # As a restaurant owner located in a popular tourist area, 
+# # I want to know the computation in this area. 
+# # I want to knows the menu of the restaurant and the ratings of the restaurant
+
+# In[ ]:
+
+
+
+
